80557c56d4f41c9adf1b08842e96353f624c81ad
suguMaranram <sugumaran.ram@aspiresys.com>
2024-02-20 14:00:51 +0530

    encap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation:ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
   title = 'encap-app';
